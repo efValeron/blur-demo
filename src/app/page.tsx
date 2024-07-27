a615427f330c72725f6d4c3abfb65f6c2d471c8d
Valeriy <masterbox06@gmail.com>
2024-07-27 13:56:21 +0500

    feat: install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className={'flex w-full items-center justify-center h-screen'}>
+    <main className={'flex h-screen w-full items-center justify-center'}>
       <h1 className={'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'}>
         Hello new Project!
       </h1>
