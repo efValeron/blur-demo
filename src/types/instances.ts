16d397ad4cde0ac97486b50ae5f96f95250d6b0d
Valeriy <masterbox06@gmail.com>
2024-08-04 16:05:56 +0500

    feat: create transaction confirmation page

--- src/types/instances.ts
+++ src/types/instances.ts
@@ -10,3 +10,16 @@ export type User = {
   preferred_username: string
   email: string
 }
+
+export type TransactionData = {
+  currency: string
+  amount: number
+  toUsername: string
+  transactionType: string
+}
+
+export type TransactionType =
+  | 'Direct_Transfer_To_Wallet'
+  | 'QR_Transfer'
+  | 'Transfer_Request'
+  | 'Bank_Card_Transfer '
